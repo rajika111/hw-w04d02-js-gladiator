4af218b3c755feb28a0ee6d3639446118b6aa6a3
RayG <54848869+rajika111@users.noreply.github.com>
2019-10-22 01:19:36 +0300

    Add files via upload

---
+++ arena.js
@@ -0,0 +1,41 @@
+class Arena{
+    constructor(name){
+        this.name=name.+name.gladiators
+        this.gladiators=[]
+
+    }
+    addgladiators(gladiators){
+        if this.gladiators.length<2{
+            this.gladiators.push(gladiators)
+        } else{
+            console.error("There are already two gladiators in the arena")
+        }
+    }
+fight (){
+    const[gladiators1,gladiators2]=this.gladiators
+    const[weapon1,weapon2]= this weapon1,weapon2
+
+    if (weapon1="TridentbeatsSpear");
+    switch (weapon2)
+    case "spear"
+    this.gladiators=[]
+    return
+    this.gladiators.pop()
+    if (weapon1="spear");
+    switch (weapon1)
+    case "SpearbeatsClubr"
+    this.gladiators=[]
+    break
+
+}
+if (weapon1="TridentbeatsSpear");
+switch (weapon2)
+ "spear"
+this.gladiators=[]
+return
+this.gladiators.pop()
+if (weapon1="spear");
+switch (weapon1)
+case "SpearbeatsClubr"
+this.gladiators=[]
+break
